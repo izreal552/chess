066f1376f192830d2c9837ead037ef97231aec91
izreal55 <izreal55@byu.edu>
2025-05-20 14:45:59 -0600

    GameHandler created (phase 3)

---
+++ server/src/main/java/server/GameHandler.java
@@ -0,0 +1,57 @@
+package server;
+
+import com.google.gson.Gson;
+import dataAccess.BadRequestException;
+import dataAccess.UnauthorizedException;
+import model.GameData;
+import model.GamesList;
+import service.GameService;
+import spark.Request;
+import spark.Response;
+
+public class GameHandler {
+    GameService gameService;
+    public GameHandler(GameService gameService){
+        this.gameService = gameService;
+    }
+
+    public Object listGames(Request request, Response response) throws UnauthorizedException{
+        String authToken = request.headers("authorization");
+        GamesList gameList = new GamesList(gameService.listGames(authToken));
+        response.status(200);
+        return new Gson().toJson(gameList);
+    }
+
+    public Object createGame(Request request, Response response) throws BadRequestException, UnauthorizedException{
+        if(!request.body().contains("\"gameName\":")){
+            throw new BadRequestException("No gameName given");
+        }
+
+        GameData gameData = new Gson().fromJson(request.body(), GameData.class);
+        String authToken = request.headers("authorization");
+        int gameID = gameService.createGame(authToken, gameData.gameName());
+        response.status(200);
+        return "{ \"gameID\": %d }".formatted(gameID);
+    }
+
+    public Object joinGame(Request req, Response resp) throws BadRequestException, UnauthorizedException {
+
+        if (!req.body().contains("\"gameID\":")) {
+            throw new BadRequestException("No gameID provided");
+        }
+
+        String authToken = req.headers("authorization");
+        record JoinGameData(String playerColor, int gameID) {}
+        JoinGameData joinData = new Gson().fromJson(req.body(), JoinGameData.class);
+        boolean joinSuccess =  gameService.joinGame(authToken, joinData.gameID(), joinData.playerColor());
+
+        if (!joinSuccess) {
+            resp.status(403);
+            return "{ \"message\": \"Error: already taken\" }";
+        }
+
+        resp.status(200);
+        return "{}";
+    }
+
+}
