5376b5226d0c854eb9ccca3f749af3c080367e79
izreal55 <izreal55@byu.edu>
2025-06-02 16:39:00 -0600

    edited main.java (phase 5)

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,15 @@
 import chess.*;
+import client.ServerFacade;
+import ui.PREloginUI;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        ServerFacade server = new ServerFacade();
+
+        PREloginUI prelogin = new PREloginUI(server);
+        prelogin.run();
+        System.out.println("Exited");
     }
 }
