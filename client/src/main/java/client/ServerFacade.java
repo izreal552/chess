5d43df2fbb2efef27e265df8a8ac6adb8b0b2560
izreal55 <izreal55@byu.edu>
2025-06-09 17:05:55 -0600

    Created websocket functions in ServerFacade (Phase 6)

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -19,6 +19,7 @@ import java.util.Map;
 public class ServerFacade {
 
     HttpCommunicator http;
+    WebsocketCommunicator ws;
     String serverDomain;
     String authToken;
 
@@ -63,4 +64,37 @@ public class ServerFacade {
         return http.joinGame(gameId, playerColor);
     }
 
+    public void connectWS() {
+        try {
+            ws = new WebsocketCommunicator(serverDomain);
+        }
+        catch (Exception e) {
+            System.out.println("Failed to make connection with server");
+        }
+    }
+
+    public void sendCommand(UserGameCommand command) {
+        String message = new Gson().toJson(command);
+        ws.sendMessage(message);
+    }
+
+    public void joinPlayer(int gameID, ChessGame.TeamColor color) {
+        sendCommand(new JoinPlayer(authToken, gameID, color));
+    }
+
+    public void joinObserver(int gameID) {
+        sendCommand(new JoinObserver(authToken, gameID));
+    }
+
+    public void makeMove(int gameID, ChessMove move) {
+        sendCommand(new MakeMove(authToken, gameID, move));
+    }
+
+    public void leave(int gameID) {
+        sendCommand(new Leave(authToken, gameID));
+    }
+
+    public void resign(int gameID) {
+        sendCommand(new Resign(authToken, gameID));
+    }
 }
